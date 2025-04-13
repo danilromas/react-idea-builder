7a0a24660a82d3c50d05114309e7ba1b6934dd78
danilromas <furiblack904@gmail.com>
2025-04-13 18:30:28 +0300

    12/141

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello')
+import express from 'express'
+
+const expressApp = express()
+expressApp.get("/pong", (req, res) => {
+    res.send("pong")
+})
+expressApp.listen(3000, () => {
+    console.info('server running on port http://localhost:3000')
+})
